006f7947a4b4cde5d173b8eb0e57fa344084f11c
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-10-25 00:45:51 +0800

    ARC

--- src/utils.mm
+++ src/utils.mm
@@ -1,10 +1,8 @@
 #ifdef DM_PLATFORM_IOS
 #include <dmsdk/sdk.h>
 #include "UIKit/UIKit.h"
-
 void DoHapticFeedbackInternal() {
 	UIImpactFeedbackGenerator *H = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
-	[H prepare];	[H impactOccurred];
-	 H = NULL;   // ARC
+	                          [H prepare];         [H impactOccurred];                                 /* ARC */
 }
 #endif
