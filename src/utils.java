fecb58f69154e96b0f1eb2d9f70d12760a40a2cd
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-10-25 21:48:57 +0800

    JVAV

--- src/utils.java
+++ src/utils.java
@@ -1,8 +1,16 @@
 package arf4.utils;
+
 import java.lang.Runnable;
 import android.app.Activity;
+
 class Haptic {
 	public static void DoHapticFeedback(final Activity A) {
-		A.getWindow().getDecorView().performHapticFeedback(0, 1);
+		A.runOnUiThread(
+			new Runnable() {
+				@Override public void run() {
+					A.getWindow().getDecorView().performHapticFeedback(0, 1);
+				}
+			}
+		);
 	}
 }
