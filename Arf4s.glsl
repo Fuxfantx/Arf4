b4dbda44affaa9487d8c0e1c6c971486f5783b3c
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2025-02-19 00:27:54 +0800

    `Arf4s` Ordering & Sort

--- Arf4s.glsl
+++ Arf4s.glsl
@@ -6,9 +6,9 @@
 /* Compute Shader for Aerials Player
  * Copyright (c) 2025- 1dealGas Project
  */
-#define FUMEN(x, T, N)    layout(set = 0, binding = x, std430)  restrict uniform _##N {T v[];} N;
-#define FUMENV(x, T, N)   layout(set = 0, binding = x, std430)  restrict buffer  _##N {T v[];} N;
-#define RUNTIME(x, T, N)  layout(set = 1, binding = x, std430)  restrict buffer  _##N {T v[];} N;
+#define FUMEN(x, T, N, size)    layout(set = 0, binding = x, std430)  restrict uniform _##N {T v[size];} N;
+#define FUMENV(x, T, N, size)   layout(set = 0, binding = x, std430)  restrict buffer  _##N {T v[size];} N;
+#define RUNTIME(x, T, N, size)  layout(set = 1, binding = x, std430)  restrict buffer  _##N {T v[size];} N;
 
 /* Easetype */
 #define  STATIC   0
@@ -40,16 +40,16 @@
  * [6] radius: [0,16) 1/4
  * [11] degree: [-2048, 2047] 2/1
  */
-FUMEN(1, uint, PointTime)   /* 16384 */
-FUMEN(2, uint, PointArg)    /* 16384 */
+FUMEN(0, uint, PointMs, 16384)
+FUMEN(1, uint, Point,   16384)
 
 /* Child (dt)
  * [5] radius: [0,8) 1/4
  * [6] initLoop: [0,1) 1/64
  * [7] deltaLoop: [-2,2) 1/32
  */
-FUMEN(3, uint, ChildDt)   /* 16384 */   // (18: ms/4) (13: absScale 1/1024 [0,8)
-FUMEN(4, uint, Child)     /* 16384 */
+FUMEN(2, uint, ChildDt, 16384)   // (18: ms/4) (13: absScale 1/1024 [0,8)
+FUMEN(3, uint, Child,   16384)
 
 /* Wish
  * [6] pointCount
@@ -58,7 +58,7 @@ FUMEN(4, uint, Child)     /* 16384 */
  * [10] childCount
  * [14] childSince
  */
-FUMEN(5, uint64_t, Wish)   /* 8192 */
+FUMEN(4, uint64_t, Wish, 8192)
 
 /* Object
  * [20] ms
@@ -70,40 +70,113 @@ FUMEN(5, uint64_t, Wish)   /* 8192 */
  * [Echo 6] initLoop: [0,1) 1/64
  * [Echo 7] deltaLoop: [-2,2) 1/32
  */
-FUMENV(6, uint64_t, Hint)   /* 8192 */
-FUMENV(7, uint64_t, Echo)   /* 8192 */
+FUMENV(5, uint64_t, Hint, 8192)
+FUMENV(6, uint64_t, Echo, 8192)
 
 /* Packed
  * [1] isDaymode
- * [1] anyPressed
- * [1] anyReleased
+ * [1] isFullScreen
+ * [1] anyTouchPressed
  * [4] validTouchCount
+ * [8] frameDeltaMs
  * [8] minDt
  * [8] maxDt
  */
-layout( push_constant, std430 ) restrict uniform _In {
-	uint   msTime, deltaTime, Packed, wishIdx, hintIdx, echoIdx;   // (10 count) (13 since)
-	float  xScale, yScale, rotSin, rotCos, sizeX, sizeY;
+layout(push_constant, std430) restrict uniform Ain {
+	uint   msTime, deltaTime, Packed, ordBase, wishIdx, hintIdx, echoIdx;   // (10 count) (13 since)
+	float  xScale, yScale, rotSin, rotCos;
 	vec2   validTouches[10];
 } In;
 
-layout( set = 1, binding = 1, std430 ) restrict buffer _Out {
-	uint  visibleWishes, visibleHints, visibleEchoes, visibleEchoHelpers, visibleAnims;
+layout(set = 1, binding = 0, std430) restrict buffer Aout {
+	uint  visibleWishes, visibleHints, visibleEchoes, visibleAnims;
 	uint  hHit, eHit, spJudged, early, late, lost;
 } Out;
 
-RUNTIME(2, vec4,  WishOut)   /* 1024: (x, y, scale, tint.w) */
-RUNTIME(3, float, HintOut)   /* 5120: [x, y, tint.r, tint.g, tint.b] */
-RUNTIME(4, float, EchoOut)   /* 3072: [x, y, scale] */
-RUNTIME(5, vec4,  EchoCol)   /* 1024: (tint.r, tint.g, tint.b, tint.w) */
-RUNTIME(6, vec2,  EchoHlp)   /* 1024: (x, y) */
-RUNTIME(7, float, AnimOut)   /* 8192: [x, y, scale, leftRotDeg, rightRotDeg] */
-RUNTIME(8, vec4,  AnimCol)   /* 2048: (tint.r, tint.g, tint.b, tint.w) */
+//-------------------------//
+
+struct VisibleWish {
+	float x, y, a, scale;
+	uint64_t ord;
+};
+struct VisibleHint {
+	float x, y, r, g, b;
+	uint64_t ord;
+};
+struct VisibleEcho {
+	float x, y, a, scale, hx, hy, r, g, b;
+	uint64_t ord;
+};
+struct Anim {
+	float x, y, r, g, b, w, lsc, rsc, ld, rd;
+	uint64_t ord;
+};
+
+shared vec4 wishOut;
+RUNTIME(1, VisibleWish, WishOut, 1024)
+RUNTIME(2, VisibleHint, HintOut, 1024)
+RUNTIME(3, VisibleEcho, EchoOut, 1024)
+RUNTIME(4, Anim, AnimOut, 2048)
 
 //-------------------------//
 
-layout( local_size_x = 8, local_size_y = 8 ) in;
+#define SORT(T, ARR, SIZE) {                                                               \
+	uint N = SIZE - 1;                                                                     \
+	N |= N >> 1;    N |= N >> 2;    N |= N >> 4;    N |= N >> 8;                           \
+	const uint elemPerIvc = (++N + 63) >> 6;                                               \
+	for (uint k = 2; k <= N; k <<= 1) {                                                    \
+		for (uint j = k >> 1; j > 0; j >>= 1) {                                            \
+			for (uint t = 0; t < elemPerIvc; ++t) {                                        \
+				const uint i = gl_LocalInvocationIndex + (t << 6), partner = i ^ j;        \
+				if (i >= N || partner <= i)  continue;                                     \
+				if (( ARR .v[i].ord  >  ARR .v[partner].ord ) == bool( i & (k >> 1) )) {   \
+					T _ = ARR .v[i];                                                       \
+					ARR .v[i] = ARR .v[partner];                                           \
+					ARR .v[partner] = _;                                                   \
+				}                                                                          \
+			}                                                                              \
+			barrier();                                                                     \
+		}                                                                                  \
+	}                                                                                      \
+}   /* Implements a Bitonic Sort */
 
+//-------------------------//
+
+layout(local_size_x = 8, local_size_y = 8) in;
 void main() {
-	HintOut.v[1] = 1.0f;
+	switch( gl_WorkGroupID.x ) {
+		/* Hint */ case 0: {
+			// Judge
+			// Output
+			barrier();
+
+			// Sort
+			if( Out.visibleHints > 0 )
+				SORT(VisibleHint, HintOut, Out.visibleHints)
+			break;
+		}
+
+		/* Echo */ case 1: {
+			// Judge
+			// Output
+			barrier();
+
+			// Sort
+			if( Out.visibleEchoes > 0 )
+				SORT(VisibleEcho, EchoOut, Out.visibleEchoes)
+			break;
+		}
+
+		/* Wish */ default: {
+			// Node
+			barrier();
+
+			// Child
+			barrier();
+
+			// Sort
+			if( gl_WorkGroupID.x == 2  &&  Out.visibleWishes > 0 )
+				SORT(VisibleWish, WishOut, Out.visibleWishes)
+		}
+	}
 }
